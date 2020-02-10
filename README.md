d81023096a1edafdb03d1b2a2cad34626ab11a0d
Singh <Gursewak.Singh@flir.com>
2020-02-10 12:58:04 -0800

    Added compareTriplets and modified simpleSum

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# hackerrank
+# Hackerrank
 HackerRank is a place where programmers from all over the world come together to solve problems in a wide range of Computer Science domains such as algorithms, machine learning, or artificial intelligence, as well as to practice different programming paradigms like functional programming.
-Here are some solutions to the selected problems from the site.
+</br>
+
+</br>
+ Here are some solutions to the selected problems from the site.
---
+++ java/hourglass.java
@@ -0,0 +1,91 @@
+/*
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+For example, given the 2D array:
+
+-9 -9 -9  1 1 1 
+ 0 -9  0  4 3 2
+-9 -9 -9  1 2 3
+ 0  0  8  6 6 0
+ 0  0  0 -2 0 0
+ 0  0  1  2 4 0
+We calculate the following  hourglass values:
+
+-63, -34, -9, 12, 
+-10, 0, 28, 23, 
+-27, -11, -2, 10, 
+9, 17, 25, 18
+Our highest hourglass value is  from the hourglass:
+
+0 4 3
+  1
+8 6 6
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class hourglass {
+
+    static int hourglassSum(int[][] arr) {
+        int[] result = new int[16];
+        int track=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if(i!=0 && j!=0 && j!=arr[0].length-1 && i!=arr[0].length-1){
+                    
+                    int temp = arr[i][j]+arr[i-1][j-1]+arr[i-1][j+1]+arr[i-1][j]+arr[i+1][j-1]+arr[i+1][j+1]+arr[i+1][j];
+                    result[track]=temp;
+                    track++; 
+                }
+            }
+        }
+        Arrays.sort(result);
+        return result[result.length-1];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
